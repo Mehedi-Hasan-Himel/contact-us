8bec9c0c5f8def0ba1aac4fff88d376759b73041
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-08-29 11:45:23 +0600

    done

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -6,18 +6,48 @@ import { FaPhoneAlt } from "react-icons/fa";
 import { HiMail } from "react-icons/hi";
 import { useState } from "react";
 
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
 const ContactForm = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [text, setText] = useState("");
 
-  const onSubmit = (event) => {
-    event.preventDefault();
+  // const onSubmit = (event) => {
+  //   event.preventDefault();
 
-    setName(event.target[0].value);
-    setEmail(event.target[1].value);
-    setText(event.target[2].value);
+  //   setName(event.target[0].value);
+  //   setEmail(event.target[1].value);
+  //   setText(event.target[2].value);
+  // };
+
+  // EmailJS Start
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setName(e.target[0].value);
+    setEmail(e.target[1].value);
+    setText(e.target[2].value);
+
+    emailjs
+      .sendForm(
+        "service_sd6fay9",
+        "template_9qxisl5",
+        form.current,
+        "iq8_xlzWnEGy_xVkc"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
+  // EmailJS End
 
   return (
     <section className={Styles.container}>
@@ -34,23 +64,41 @@ const ContactForm = () => {
           text="VIA EMAIL FORM"
           icon={<HiMail fontSize={"24px"} />}
         />
-        <form onSubmit={onSubmit}>
+        {/* <form ref={form} onSubmit={sendEmail}>
           <div className={Styles.form_control}>
             <label htmlFor="name">Name</label>
-            <input type="text" name="name" />
+            <input type="text" name="user_name" />
           </div>
           <div className={Styles.form_control}>
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" />
+            <input type="email" name="user_email" />
           </div>
           <div className={Styles.form_control}>
-            <label htmlFor="text">Text</label>
+            <label htmlFor="text">Message</label>
             <textarea name="text" rows={8} />
           </div>
           <div style={{ display: "flex", justifyContent: "end" }}>
-            <Button text="Submit Button" />
+            <input type="submit" value="Send" />
           </div>
           <div>{name + " " + email + " " + text}</div>
+        </form> */}
+        <form ref={form} onSubmit={sendEmail}>
+          <div className={Styles.form_control}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+          </div>
+
+          <div className={Styles.form_control}>
+            <label>Email</label>
+            <input type="email" name="user_email" />
+          </div>
+          <div className={Styles.form_control}>
+            <label>Message</label>
+            <textarea name="message" />
+          </div>
+          <div style={{ display: "flex", justifyContent: "end" }}>
+            <input type="submit" value="Send" />
+          </div>
         </form>
       </section>
       <section className={Styles.contact_Img}>
